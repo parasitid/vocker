e936e96e87d29fc8ceaee57f5c531ab13b27fdca
Yann DEGAT <yann.degat@numergy.com>
2014-10-17 15:22:22 +0200

    add JAVA_HOME check

--- src/scripts/vertx_run.sh
+++ src/scripts/vertx_run.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "running verticle {{ VERTICLE_NAME }}~{{ VERTICLE_VERSION }} on port {{ VERTICLE_PORT }}"
-/root/.gvm/vertx/current/bin/vertx runzip /opt/{{ VERTICLE_NAME }}~{{ VERTICLE_VERSION }}.zip
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+
+if [ ! -x $JAVA_HOME/bin/javac ]; then 
+   echo "no proper jdk found. check install. exiting"
+   exit 1
+fi
+
+/root/.gvm/vertx/{{ VERTX_VERSION }}/bin/vertx runzip /opt/{{ VERTICLE_NAME }}~{{ VERTICLE_VERSION }}.zip
